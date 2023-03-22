b83a6da7e0e24680b395d47ddc98d35f36b5e664
John Reyes <jhonfer96@hotmail.com>
2023-03-22 15:15:50 -0500

    section three: add arrow function

--- 02-fundamentos/flecha.js
+++ 02-fundamentos/flecha.js
@@ -1,8 +1,8 @@
 // function sumar( a, b ) {
 //     return a + b;
 // }
-const sumar = (a, b = 10) => {
-    return a + b;
-}
+const sumar = (a, b) => a + b;
+const saludar = () => 'Hola mundo';
 
-console.log(sumar(5));
+console.log(sumar(5, 10));
+console.log(saludar());
