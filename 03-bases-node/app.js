877a42d6c47cb6361e7911c781ca1c1df45ebf2d
John Reyes <jhonfer96@hotmail.com>
2023-03-23 16:33:22 -0500

    section four: add file system library

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 // 5 x 1 = 5
 // 5 x 2 = 10
 
@@ -6,7 +8,17 @@ console.log('=================');
 console.log('  Tabla del: 5   ');
 console.log('=================');
 
-const base = 5;
+const base = 3; 
+let salida = '';
+
 for(let i = 1; i <= 10; i++) {
-    console.log(`${ base } x ${ i } = ${ base * i }`);
+    salida += `${ base } x ${ i } = ${ base * i }\n`;
 }
+
+console.log(salida);
+
+fs.writeFile(`tabla-${ base }.txt`, salida, (err) => {
+    if (err) throw err;
+
+    console.log('tabla-5.txt creado');
+});
