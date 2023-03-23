4f1625c412e61c1f4df8579b2de3a3b00e897fa9
John Reyes <jhonfer96@hotmail.com>
2023-03-23 16:54:40 -0500

    section four: import files to project and add promise

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,24 +1,12 @@
-const fs = require('fs');
+const { crearArchivo } = require('./helpers/multiplicar');
 
 // 5 x 1 = 5
 // 5 x 2 = 10
 
 console.clear();
-console.log('=================');
-console.log('  Tabla del: 5   ');
-console.log('=================');
 
 const base = 3; 
-let salida = '';
 
-for(let i = 1; i <= 10; i++) {
-    salida += `${ base } x ${ i } = ${ base * i }\n`;
-}
-
-console.log(salida);
-
-fs.writeFile(`tabla-${ base }.txt`, salida, (err) => {
-    if (err) throw err;
-
-    console.log('tabla-5.txt creado');
-});
+crearArchivo(base)
+    .then( nombreArchivo => console.log(nombreArchivo, 'creado'))
+    .catch( err => console.log(err));
