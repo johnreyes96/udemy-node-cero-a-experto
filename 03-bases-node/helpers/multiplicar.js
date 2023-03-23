f80bf808ca4bb992f8bc2907f5ee1d760573fbf1
John Reyes <jhonfer96@hotmail.com>
2023-03-23 16:57:37 -0500

    section four: another way to handling promises with async

--- 03-bases-node/helpers/multiplicar.js
+++ 03-bases-node/helpers/multiplicar.js
@@ -1,22 +1,24 @@
 const fs = require('fs');
 
-const crearArchivo = ( base = 5 ) => {
-    return new Promise( (resolve, rejected) => {
+const crearArchivo = async( base = 5 ) => {
+    try {
         console.log('=================');
         console.log('  Tabla del:', base);
         console.log('=================');
-    
+
         let salida = '';
-    
+
         for(let i = 1; i <= 10; i++) {
             salida += `${ base } x ${ i } = ${ base * i }\n`;
         }
-    
+
         console.log(salida);
-    
+
         fs.writeFileSync(`tabla-${ base }.txt`, salida);
-        console.log(`tabla-${ base }.txt creado`);
-    });
+        return `tabla-${ base }.txt creado`;
+    } catch (err) {
+        throw err
+    }
 }
 
 module.exports = {
