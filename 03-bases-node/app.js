84d848814a3e63630c67a44a4e538ee3e4cf4ec2
John Reyes <jhonfer96@hotmail.com>
2023-03-24 01:15:23 -0500

    section four: add params to execution main

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -4,8 +4,12 @@ const { crearArchivo } = require('./helpers/multiplicar');
 // 5 x 2 = 10
 
 console.clear();
+const [, , arg3 = 'base=5'] = process.argv
+const [ , base = 5 ] = arg3.split('=');
+// exec as node app --base=5
+console.log(base);
 
-const base = 3; 
+// const base = 5; 
 
 crearArchivo(base)
     .then( nombreArchivo => console.log(nombreArchivo, 'creado'))
