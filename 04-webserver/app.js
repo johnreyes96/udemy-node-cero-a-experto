3f34770ccee209f2d7cc98fbdd2908c5000dca27
John Reyes <jhonfer96@hotmail.com>
2023-03-27 16:37:45 -0500

    section seven: add args from controller to view

--- 04-webserver/app.js
+++ 04-webserver/app.js
@@ -9,7 +9,10 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {
+        nombre: 'John Reyes',
+        titulo: 'Curso de Node'
+    });
 })
 app.get('/generic', (req, res) => {
     res.sendFile(__dirname + '/public/generic.html')
