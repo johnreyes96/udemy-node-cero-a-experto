24dc9e8973d9d3a75c61719683af57dd8d64887a
John Reyes <jhonfer96@hotmail.com>
2023-03-27 16:33:04 -0500

    section seven: add handlebars dependency and render home

--- 04-webserver/app.js
+++ 04-webserver/app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 8080;
+
+// TODO: require('hbs');
+app.set('view engine', 'hbs');
 
 // Servir contenido estático
-app.use(express.static('public'))
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.render('home');
+})
 app.get('/generic', (req, res) => {
     res.sendFile(__dirname + '/public/generic.html')
 })
