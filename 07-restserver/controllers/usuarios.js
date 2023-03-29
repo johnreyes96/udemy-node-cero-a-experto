d553d35c82d1f23d4091d504eee943cd9e5d5b3c
John Reyes <jhonfer96@hotmail.com>
2023-03-29 10:52:33 -0500

    section eight: get body in post endpoint or another methods

--- 07-restserver/controllers/usuarios.js
+++ 07-restserver/controllers/usuarios.js
@@ -7,8 +7,12 @@ const usuariosGet = (req, res = response) => {
 }
 
 const usuariosPost = (req, res = response) => {
+    const { nombre, edad } = req.body;
+
     res.json({
-        msg: "post API - controlador"
+        msg: "post API - controlador",
+        nombre,
+        edad
     });
 }
 
