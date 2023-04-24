d40446040a703624fa36e40dff5f281137c10db2
John Reyes <jhonfer96@hotmail.com>
2023-04-23 23:01:20 -0500

    section nine: add express-validator dependency and add email field obligatory

--- 07-restserver/controllers/usuarios.js
+++ 07-restserver/controllers/usuarios.js
@@ -1,6 +1,7 @@
 const { request, response } = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
+const { validationResult } = require('express-validator');
 
 const usuariosGet = (req = request, res = response) => {
     const { q, nombre = 'No name', apikey, page = 1, limit } = req.query
@@ -15,10 +16,21 @@ const usuariosGet = (req = request, res = response) => {
 }
 
 const usuariosPost = async(req, res = response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors);
+    }
+
     const { nombre, correo, password, rol } = req.body;
     const usuario = new Usuario({ nombre, correo, password, rol });
 
     // Verificar si el correo existe
+    const existeEmail = await Usuario.findOne({ correo });
+    if (existeEmail) {
+        return res.status(400).json({
+            msg: 'Ese correo ya está registrado'
+        });
+    }
 
     // Encriptar la contraseña
     const salt = bcryptjs.genSaltSync();
--- 07-restserver/routes/usuarios.js
+++ 07-restserver/routes/usuarios.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { usuariosGet,
         usuariosPut,
         usuariosPatch,
@@ -8,7 +9,9 @@ const { usuariosGet,
 const router = Router();
 
 router.get('/', usuariosGet);
-router.post('/', usuariosPost);
+router.post('/', [
+    check('correo', 'El correo no es válido').isEmail()
+], usuariosPost);
 router.put('/:id', usuariosPut);
 router.patch('/', usuariosPatch);
 router.delete('/', usuariosDelete);
