76787946c31d5c9b4b0d495daa70d16661e71d0f
John Reyes <jhonfer96@hotmail.com>
2023-04-23 23:27:29 -0500

    section nine: add role validator using database

--- 07-restserver/routes/usuarios.js
+++ 07-restserver/routes/usuarios.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
+const Role = require('../models/role');
 
 const { validarCampos } = require('../middlewares/validar-campos');
 const { usuariosGet,
@@ -15,7 +16,12 @@ router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password debe ser más de 6 letras').isLength(min = 6),
     check('correo', 'El correo no es válido').isEmail(),
-    check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    check('rol').custom( async(rol = '') => {
+        const existeRol = await Role.findOne({ rol });
+        if (!existeRol) {
+            throw new Error(`El rol ${ rol } no está registrado en la BD`);
+        }
+    }),
     validarCampos
 ], usuariosPost);
 router.put('/:id', usuariosPut);
