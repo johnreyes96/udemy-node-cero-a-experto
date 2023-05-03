1869750c359b8d8064f6ee29150c9e5c1dc14787
John Reyes <jhonfer96@hotmail.com>
2023-05-03 08:32:03 -0500

    section ten: login user

--- 07-restserver/controllers/auth.js
+++ 07-restserver/controllers/auth.js
@@ -1,9 +1,44 @@
 const { response } = require("express");
+const Usuario = require('../models/usuario');
+const bcryptjs = require('bcryptjs');
 
-const login = (req, res = response) => {
-    res.json({
-        msg: 'Login ok'
-    });
+const login = async(req, res = response) => {
+    const { correo, password } = req.body;
+
+    try {
+        // Verificar si el email existe
+        const usuario = await Usuario.findOne({ correo });
+        if (!usuario) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos'
+            });
+        }
+
+        // Si el usuario está activo
+        if (!usuario.estado) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos'
+            });
+        }
+
+        // Verificar la contraseña
+        const validPassword = bcryptjs.compareSync(password, usuario.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos'
+            });
+        }
+
+        // Generar el JWT (Json Web Token)
+        res.json({
+            msg: 'Login ok'
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Hable  con el administrador'
+        });
+    }
 }
 
 module.exports = {
