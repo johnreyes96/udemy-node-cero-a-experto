8f766deb97b07ce5d7c33971026f4036d9979ca5
John Reyes <jhonfer96@hotmail.com>
2023-05-16 14:45:18 -0500

    section eleven: validate google token

--- 07-restserver/controllers/auth.js
+++ 07-restserver/controllers/auth.js
@@ -1,8 +1,9 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async(req, res = response) => {
     const { correo, password } = req.body;
@@ -48,10 +49,20 @@ const login = async(req, res = response) => {
 
 const googleSingIn = async(req, res = response) => {
     const { id_token } = req.body;
-    res.json({
-        msg: 'Todo ok',
-        id_token
-    });
+
+    try {
+        const googleUser = await googleVerify(id_token);
+
+        res.json({
+            msg: 'Todo bien! google signin',
+            id_token
+        });
+    } catch (error) {
+        json.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        });
+    }
 }
 
 module.exports = {
