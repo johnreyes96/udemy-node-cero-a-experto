f5a6bf0b557fe77ca7d2fe9e17e2f08312dc4cac
John Reyes <jhonfer96@hotmail.com>
2023-05-18 08:26:14 -0500

    section twelve: create category

--- 07-restserver/routes/categorias.js
+++ 07-restserver/routes/categorias.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT, validarCampos } = require('../middlewares');
+const { crearCategoria } = require('../controllers/categorias');
 
 const router = Router();
 
@@ -16,9 +17,11 @@ router.get('/:id', (req, res) => {
 });
 
 // Crear una categoria - privado - cualquier persona con un token válido
-router.post('/', (req, res) => {
-    res.json('post');
-});
+router.post('/', [
+    validarJWT,
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    validarCampos,
+], crearCategoria);
 
 // Actualizar - privado - cualquier persona con un token válido
 router.put('/:id', (req, res) => {
